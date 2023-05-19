ad0a70da2ae8d3a8173b8d0335fcec6bfd5f4d52
Fenush <econtegarcia@gmail.com>
2023-05-19 01:41:10 -0300

    Basic logKey1() Added for Testing

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // operations f() start here
-let firstNumber,
-    secondNumber,
-    result,
-    storedResult; 
+let firstNumber, secondNumber, result, storedResult;
 
 let operations = {
-    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
-    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
-    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
-    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
 };
 
 operate = function (value1, value2, operator) {
-    if (operator in operations) {
-        return result = operations[operator](value1, value2);
-    }
+  if (operator in operations) {
+    return (result = operations[operator](value1, value2));
+  }
+};
+// keysLogger start here
+const numbers = {"0":"0","1":"1","2":"2","3":"3","4":"4","5":"5","6":"6","7":"7","8":"8","9":"9","0":"0",".":"."};
+//const operators = {"+":"+","-":"-","*":"*","/":"/"};
+let a="";
+let operator;
+
+function logKey1 (e){
+if (e.key in numbers){
+a= a + e.key;}
+console.log(a);
 }
-// operations f() ends here
+
+document.addEventListener("keydown", logKey1);
