58d102f2cd6a538244d9b932235916a5148b4e5c
Fenush <econtegarcia@gmail.com>
2023-05-19 11:39:04 -0300

    logKey1() is Working now and logkey2()Added

--- script.js
+++ script.js
@@ -35,18 +35,54 @@ numberConstructor();
 const operators = { "+": "+", "-": "-", "*": "*", "/": "/" };
 let a = "";
 let operator;
-let lastKey;
+let b = "";
 
 function logKey1(e) {
   if (!operator) {
-   
+    if (e.key in operators && a === "") { // if operator comes first then 1stNumber
+      a = "0";
+      operator = e.key;
+      console.log("l45")
+      console.log("a " + a);
+      console.log("operator " + operator);
+      return;
+    }
+    if (e.key in operators) {  // operator comes before 1stNumber is assigned
+      operator = e.key;
+      console.log("l52")
+      console.log("a " + a);
+      console.log("operator " + operator);
+      return;
+    }
+    if (e.key in numbers) { // 1st number is at his normal position
+      a = a + e.key;
+      console.log("l59")
+      console.log("a " + a);
+      console.log("operator " + operator);
+      return;
+    }
+  } else if (e.key in operators) { // if operator is assigned more then once op=op
+    operator = e.key;
+    console.log("l66")
+    console.log("a " + a);
+    console.log("operator " + operator);
+    return;
+  } else if (e.key in numbers) { // if 1stNumber is assigned and Op, 1ndNumber is assigned
+    b = e.key;
+    document.removeEventListener("keydown", logKey1);
+    document.addEventListener("keydown", logKey2);
+    console.log("l74")
+    console.log("a " + a);
+    console.log("b " + b);
+    console.log("operator " + operator);
   }
 }
+
 document.addEventListener("keydown", logKey1);
 
-function logKey2 (e) {
+function logKey2(e) {
   console.log("hello");
-  console.log("a="+a);
-  console.log("operator="+ operator)
-
+  console.log("a " + a);
+  console.log("operator " + operator);
+  console.log("a=" + b);
 }
